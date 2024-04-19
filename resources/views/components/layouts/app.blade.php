09d783ba590d5e03d2ec308be79179643edfaca8
Steve Creekmore <Creekmore108@gmail.com>
2024-04-19 09:55:06 -0500

    added alpine CDN

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         <script src="https://cdn.tailwindcss.com"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
